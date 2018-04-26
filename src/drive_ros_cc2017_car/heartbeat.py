2574fbd2615f5e726d729b5d7ba81b5858ba5e0e
Andreas Dolp <toseban@gmail.com>
2018-04-26 15:59:06 +0200

    cleanup and add launch file

--- src/drive_ros_cc2017_car/heartbeat.py
+++ src/drive_ros_cc2017_car/heartbeat.py
@@ -1,11 +1,4 @@
-import os
-import rospy
-import rospkg
-
 from qt_gui.plugin import Plugin
-from python_qt_binding import loadUi
-#from python_qt_binding.QtGui import QWidget
-from python_qt_binding.QtWidgets import QWidget
 
 from .heartbeat_widget import HeartbeatWidget
 
@@ -31,24 +24,13 @@ class HeartbeatVisualization(Plugin):
 
         # Create QWidget
         self._widget = HeartbeatWidget()
-        # Get path to UI file which should be in the "resource" folder of this package
-   #     ui_file = os.path.join(rospkg.RosPack().get_path('rqt_mypkg'), 'resource', 'heartbeat.ui')
-        # Extend the widget with all attributes and children from UI file
-   #     loadUi(ui_file, self._widget)
-        # Give QObjects reasonable names
-   #     self._widget.setObjectName('HeartbeatVisualizationUi')
-        # Show _widget.windowTitle on left-top of each plugin (when 
-        # it's set in _widget). This is useful when you open multiple 
-        # plugins at once. Also if you open multiple instances of your 
-        # plugin at once, these lines add number to make it easy to 
-        # tell from pane to pane.
+
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         # Add widget to the user interface
         context.add_widget(self._widget)
 
     def shutdown_plugin(self):
-        # TODO unregister all publishers here
         self._widget.shutdown_plugin()
 
     def save_settings(self, plugin_settings, instance_settings):
@@ -56,9 +38,3 @@ class HeartbeatVisualization(Plugin):
 
     def restore_settings(self, plugin_settings, instance_settings):
         self._widget.restore_settings(plugin_settings, instance_settings)
-
-    #def trigger_configuration(self):
-        # Comment in to signal that the plugin has a way to configure
-        # This will enable a setting button (gear icon) in each dock widget title bar
-        # Usually used to open a modal configuration dialog
-
--- src/drive_ros_cc2017_car/heartbeat_widget.py
+++ src/drive_ros_cc2017_car/heartbeat_widget.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os
+from os.path import join
 
 import rospkg
 from PyQt5 import QtGui
@@ -9,46 +9,42 @@ import PyQt5.uic
 import rospy
 from PyQt5.QtWidgets import QLabel, QWidget
 
-#from qt_gui.plugin import Plugin
-#from python_qt_binding import loadUi
-#from python_qt_binding.QtGui import QWidget
-#from python_qt_binding.QtWidgets import QWidget
-
 from drive_ros_msgs.msg import mav_cc16_HEARTBEAT
 
 class HeartbeatWidget(QWidget):
 
-    #TODO entfernen?
-    _heartbeat_attributes = ('battery_voltage', 'drive_mode', 'rc_steering_front', 'rc_steering_rear', 'remote_control')
+    #TODO get from somewhere else?
     remote_control_status = ('DISCONNECTED', 'CONNECTED','AUTONOMOUS', 'SEMI_AUTONOMOUS', 'MANUAL')
-    drive_mode_status = ('TRACK', 'TRACK_OBSTACLES',
-                         'PARKING', 'IDLE')
+    drive_mode_status = ('TRACK', 'TRACK_OBSTACLES', 'PARKING', 'IDLE')
 
     def callback(self, heartbeat):
+        # battery
         if self.old_battery_voltage != heartbeat.battery_voltage:
-            self.batterybar.setValue(heartbeat.battery_voltage) #TODO sometimes not set?
-            self.battery_voltage_displayValue.setText(str(heartbeat.battery_voltage))
+            self.batterybar.setValue(heartbeat.battery_voltage)
+            self.battery_voltage_displayValue.setText(str(heartbeat.battery_voltage) + " mV")
             self.old_battery_voltage = heartbeat.battery_voltage
 
+        # velocity
         if self.old_speed != heartbeat.rc_velocity:
             speed = int(abs(heartbeat.rc_velocity) * 100)
-            # TODO speedbar.setValue() crashes whole thing
-            #self.speedbar.setValue(speed) #TODO m/s
-            self.rc_velocity_displayValue.setText(str(heartbeat.rc_velocity))
+            self.speedbar.setValue(speed)
+            self.rc_velocity_displayValue.setText("%.3f m/s" % heartbeat.rc_velocity)
             self.old_speed = heartbeat.rc_velocity
 
+        # drive mode
         if self.old_drive_mode != heartbeat.drive_mode:
             self.drive_mode_displayValue.setText(str(heartbeat.drive_mode) + " - " + self.drive_mode_status[heartbeat.drive_mode])
             self.old_drive_mode = heartbeat.drive_mode
 
+        # remote control status
         if self.old_remote_control != heartbeat.remote_control:
             self.remote_control_displayValue.setText(str(heartbeat.remote_control) + " - " + self.remote_control_status[heartbeat.remote_control])
             self.old_remote_control = heartbeat.remote_control
 
-        #tires TODO dont draw new every time
+        # tires
         # front wheels
         if self.old_front_angle != heartbeat.rc_steering_front:
-            self.rc_steering_front_displayValue.setText(str(heartbeat.rc_steering_front))
+            self.rc_steering_front_displayValue.setText("%.3f rad" % heartbeat.rc_steering_front)
             tr = QtGui.QTransform()
             tr.rotateRadians(heartbeat.rc_steering_front)
             rotated = self.tire.transformed(tr)
@@ -57,7 +53,7 @@ class HeartbeatWidget(QWidget):
             self.old_front_angle = heartbeat.rc_steering_front
         # rear wheels
         if self.old_rear_angle != heartbeat.rc_steering_rear:
-            self.rc_steering_rear_displayValue.setText(str(heartbeat.rc_steering_rear))
+            self.rc_steering_rear_displayValue.setText("%.3f rad" % heartbeat.rc_steering_rear)
             tr = QtGui.QTransform()
             tr.rotateRadians(heartbeat.rc_steering_rear)
             rotated = self.tire.transformed(tr)
@@ -65,56 +61,59 @@ class HeartbeatWidget(QWidget):
             self.left_rear_wheel.setPixmap(rotated)
             self.old_rear_angle = heartbeat.rc_steering_rear
 
-        #show direction TODO doesn't hide correctly
-        if heartbeat.rc_velocity > 0:
+        # direction
+        if heartbeat.rc_velocity > 0 and self.old_direction != 1:
             self.driving_direction_backwards.hide()
             self.driving_direction_forward.show()
-        elif heartbeat.rc_velocity < 0:
+            self.old_direction = 1
+        elif heartbeat.rc_velocity < 0 and self.old_direction != -1:
             self.driving_direction_forward.hide()
             self.driving_direction_backwards.show()
-        else:
+            self.old_direction = -1
+        elif heartbeat.rc_velocity == 0 and self.old_direction != 0:
             self.driving_direction_backwards.hide()
             self.driving_direction_forward.hide()
+            self.old_direction = 0
 
     def __init__(self):
 
         super(HeartbeatWidget, self).__init__()
 
-        # old values
-        self.old_front_angle = float(0)
-        self.old_rear_angle = float(0)
+        # init old values
+        # sometimes invalid value, so it's set on first message
+        self.old_front_angle = float(-1)
+        self.old_rear_angle = float(-1)
         self.old_battery_voltage = 0
-        self.old_speed = float(0)
-        self.old_drive_mode = 0
-        self.old_remote_control = 0
+        self.old_speed = float(-1)
+        self.old_drive_mode = -1
+        self.old_remote_control = -1
         self.old_direction = 0  # 0 = stop, -1 = backwards, 1 = forwards
 
 
-        # TODO PATH
-        ui_file = os.path.join(rospkg.RosPack().get_path('drive_ros_cc2017_car'), 'resource', 'heartbeat.ui')
+        # ui
+        ui_file = join(rospkg.RosPack().get_path('drive_ros_cc2017_car'), 'resource', 'heartbeat.ui')
         PyQt5.uic.loadUi(ui_file, self)
 
         # Give QObjects reasonable names
         self.setObjectName('HeartbeatVisualizationUi')
 
-        # TODO PATH
-        self.tire_file = os.path.join(rospkg.RosPack().get_path('drive_ros_cc2017_car'), 'resource', 'tire_small.png')
+        # init widgets
+        self.tire_file = join(rospkg.RosPack().get_path('drive_ros_cc2017_car'), 'resource', 'tire_small.png')
         self.tire = QtGui.QPixmap(self.tire_file)
         self.right_front_wheel.setPixmap(self.tire)
         self.left_front_wheel.setPixmap(self.tire)
         self.right_rear_wheel.setPixmap(self.tire)
         self.left_rear_wheel.setPixmap(self.tire)
-        # TODO add speedometer widget in heartbeat.ui with sip?
 
         self.speedbar = self.rc_velocity_progressBar
-        self.speedbar.setRange(0,200) #TODO get from launch file
+        self.speedbar.setRange(0,rospy.get_param("~max_velocity"))
 
         self.batterybar = self.battery_voltage_progressBar
-        self.batterybar.setRange(0,10000) #TODO get from launch file
+        self.batterybar.setRange(0,rospy.get_param("~max_voltage"))
 
         # direction
-        self.arrow_file = os.path.join(rospkg.RosPack().get_path('drive_ros_cc2017_car'), 'resource', 'arrow.png')
-        direction = QtGui.QPixmap(self.arrow_file)  # TODO set in launch file
+        self.arrow_file = join(rospkg.RosPack().get_path('drive_ros_cc2017_car'), 'resource', 'arrow.png')
+        direction = QtGui.QPixmap(self.arrow_file)
         self.driving_direction_forward.setPixmap(direction)
         self.driving_direction_forward.hide()
         tr = QtGui.QTransform()
@@ -122,16 +121,15 @@ class HeartbeatWidget(QWidget):
         self.driving_direction_backwards.setPixmap(direction.transformed(tr))
         self.driving_direction_backwards.hide()
 
-        global _heartbeat_sub
-        _heartbeat_sub = rospy.Subscriber("/from_mav/heartbeat", mav_cc16_HEARTBEAT, self.callback)
+        # subscribe to heartbeat
+        self._heartbeat_sub = rospy.Subscriber("/from_mav/heartbeat", mav_cc16_HEARTBEAT, self.callback)
 
     def shutdown_plugin(self):
-        global _heartbeat_sub
-        _heartbeat_sub.unregister()
+        self._heartbeat_sub.unregister()
 
     def save_settings(self, plugin_settings, instance_settings):
-        pass #TODO?
+        pass
 
     def restore_settings(self, plugin_settings, instance_settings):
-        pass #TODO?
+        pass
 
